2b0ca9c40fbbaea42b622dfa86805d1a8639b3aa
Isaac Liu <isliu@stu.naperville203.org>
2024-08-26 14:14:08 -0500

    h

--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -9,6 +9,13 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
-        
+        // The addLast method can be used to populate a list
+        LinkedList<String> staff = new LinkedList<>();
+        staff.addLast("Tony");
+        staff.addLast("Natasha");
+        staff.addLast("Peter");
+        staff.addLast("Gamora");
+
+        System.out.println(staff);
     }
 }
