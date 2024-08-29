18470ffec99d5317e098d044dbe1788ad664625a
ZaoShang12 <142527679+ZaoShang12@users.noreply.github.com>
2024-08-29 15:20:18 -0500

    finished reverse

--- Chapter 15 Activities/Reverse/src/ListUtil.java
+++ Chapter 15 Activities/Reverse/src/ListUtil.java
@@ -12,12 +12,19 @@ public class ListUtil
     */
     public static void reverse(LinkedList<String> strings)
     {
-        LinkedList<String> strings2 = new LinkedList<>();
-        ListIterator<String> iterator = strings.listIterator();
-        strings2.add(null)
-        while(iterator.hasNext()){
-            
+        LinkedList<String> strings2 = new LinkedList<>(); // new reversed version of strings
+        ListIterator<String> iterator = strings.listIterator(); // iterator
 
+        // strings2 stores elements of strings in reverse order
+        while(iterator.hasNext()){
+            strings2.add(strings.removeLast());
+           
+        }
+        strings.removeAll(strings); // clears all elements in strings
+        iterator = strings2.listIterator(); // now points iterator to strings 2 
+        while(iterator.hasNext()){
+            strings.add(iterator.next()); // transfers elements and their order to the original linked list strings
+           
         }
     }
 }
