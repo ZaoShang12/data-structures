cb7a72c9c0e69b2a76a32c266217252c0538724e
ZaoShang12 <isliu@stu.naperville203.org>
2024-09-20 14:17:31 -0500

    finished sentence reverser

--- Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
+++ Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
@@ -15,9 +15,31 @@ public class SentenceReverser
     public static String reverse(String sentence)
     {
     	Scanner scanner = new Scanner(sentence);
-    	
+    	Stack<String> st = new Stack<>();
+        String result = new String();
         // Complete this method. Use a Stack.
-        ...
+        
+        while(scanner.hasNext()){
+            String word = scanner.next();
+            
+            
+            if(word.indexOf(".") == -1){
+                st.push(word);
+            }
+            
+            else{
+                st.push(word.substring(0, word.length()-1));
+                
+                sentence = sentence.substring(sentence.indexOf(".")+1);
+                String w = st.pop();
+                result = result + " " + (w.substring(0,1).toUpperCase()) + w.substring(1) ;
+                while(!st.isEmpty()){
+                    result = result + " "+st.pop().toLowerCase();
+                }
+                System.out.print(".");
+            }
+        } 
+        return result;
 
 
 
